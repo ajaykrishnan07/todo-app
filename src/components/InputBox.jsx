b76f94088c78b8268a602a74b2210ae2b1c93ca3
ajaykrishnan07 <ajkrish2002@gmail.com>
2023-09-07 00:30:37 +0530

    On click button working

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export const InputBox = () => {
+  const inputref = useRef(null);
+  const handleClick = () => {
+    console.log(inputref.current.value);
+  };
+
   return (
-    <input
-      type="text"
-      className="border border-black rounded-md w-3/6 h-16 text-2xl"
-      placeholder="Enter your task"
-    ></input>
+    <div>
+      <div className="h-screen flex items-center justify-center">
+        <input
+          type="text"
+          className="border border-black rounded-md w-3/6 h-16 text-2xl"
+          ref={inputref}
+        ></input>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-16 ml-2 border border-blue-700 rounded"
+          onClick={handleClick}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
   );
 };
